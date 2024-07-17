73edefb35f88cc000bf79ac9e9d4a5c85d59b106
TheSakyo <sacha.cardone@gmail.com>
2024-07-17 23:21:38 +0200

    Création d'une application API REST pour mon portfolio.
    Mise en place des différentes entités, Mise en place des Repository, Services, DTOs, etc....
    
    [TO DO : Finir la mise en place des services et attaquer la partie controller]

---
+++ src/main/java/fr/thesakyo/portfolioapi/PortFolioApiApplication.java
@@ -0,0 +1,16 @@
+package fr.thesakyo.portfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortFolioApiApplication {
+
+	/**************************************************************************/
+	/************   ⬇️    POINT D'ENTRÉE DE L'APPLICATION    ⬇️   ************/
+	/*************************************************************************/
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortFolioApiApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/thesakyo/portfolioapi/PortFolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.thesakyo.portfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
