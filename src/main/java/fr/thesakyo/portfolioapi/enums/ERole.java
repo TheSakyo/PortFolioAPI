27240ae53eeacce136395f1a29f9441a7dac27c8
TheSakyo <sacha.cardone@gmail.com>
2024-10-12 18:20:24 +0200

    Ajout de la méthode ToString() pour donner une chaîne de caractères précis aux rôles énumérés

--- src/main/java/fr/thesakyo/portfolioapi/enums/ERole.java
+++ src/main/java/fr/thesakyo/portfolioapi/enums/ERole.java
@@ -5,15 +5,42 @@ public enum ERole {
     /**
      * Rôle SUPERADMIN.
      */
-    ROLE_SUPERADMIN,
+    ROLE_SUPERADMIN {
+
+        /**
+         * Converti un l'{@link ERole énumération} en {@link String chaîne de caractères}.
+         *
+         * @return Une {@link String chaîne de caractères} de notre {@link ERole énumération}.
+         */
+        @Override
+        public String toString() {  return "SUPERADMIN"; }
+    },
 
     /**
      * Rôle ADMIN.
      */
-    ROLE_ADMIN,
+    ROLE_ADMIN {
+
+        /**
+         * Converti un l'{@link ERole énumération} en {@link String chaîne de caractères}.
+         *
+         * @return Une {@link String chaîne de caractères} de notre {@link ERole énumération}.
+         */
+        @Override
+        public String toString() {  return "ADMIN"; }
+    },
 
     /**
      * Rôle UNKNOWN.
      */
-    ROLE_UNKNOWN,
+    ROLE_UNKNOWN {
+
+        /**
+         * Converti un l'{@link ERole énumération} en {@link String chaîne de caractères}.
+         *
+         * @return Une {@link String chaîne de caractères} de notre {@link ERole énumération}.
+         */
+        @Override
+        public String toString() {  return "UNKNOWN"; }
+    },
 }
